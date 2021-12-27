75a6f3d2fe6a7eacfea68ff24ac3676e304f9fae
ADMIN <ADMIN@MSI>
2021-12-27 16:26:30 +0700

    Add project files.

---
+++ PhamNhuViet_BAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PhamNhuViet_BAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ PhamNhuViet_DTO/DTO/EmployeeDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhamNhuViet_DTO.DTO
+{
+    public class EmployeeDTO
+    {
+        public string IdEmployee { get; set; }
+        public string Name { get; set; }
+        public string DateBirth { get; set; }
+        public string Gender { get; set; }
+        public string PlaceBirth { get; set; }
+        public string IdDepartment { get; set; }
+    }
+}
